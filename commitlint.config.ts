814e26fb73dee3bb3a23ffd2fbeffcbe0c3354d3
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-02-26 01:21:13 +0900

    init: commitlint, lint-staged, husky 초기 세팅

---
+++ commitlint.config.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "remove",
+        "style",
+        "chore",
+        "init",
+        "refactor",
+        "test",
+        "revert",
+        "docs",
+        "ci"
+      ]
+    ]
+  }
+};
