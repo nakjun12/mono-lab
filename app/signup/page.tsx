7aee37ad9333d27cd2bb55f7c6f6f1417df7f160
Turbobot <turbobot@vercel.com>
2024-02-28 12:18:13 +0900

    feat(create-turbo): apply official-starter transform

--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "@/app/ui/form/sign-up-form";
-
-export default function SignUpPage() {
-  return (
-    <main className="flex items-center justify-center md:h-screen">
-      <SignUpForm />
-    </main>
-  );
-}
--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["cdn.builder.io"] // 이 부분에 'cdn.builder.io'를 추가합니다.
-  }
-};
-export default nextConfig;
