c88bcc67f14f300fb3a92a376930bdb664c7e740
anyl92 <yulim9592@gmail.com>
2024-03-01 18:51:10 +0900

    feat(footer): 폴더구조 변경, 타입 추가, 필요없는 상태 제거, 전체 레이아웃에 푸터 추가

---
+++ apps/flower-house/app/_shared/_components/footer.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Image from "next/image";
+import { IconNames } from "../_types/footer";
+import { useRouter, usePathname } from "next/navigation";
+
+const Footer: React.FC = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const imageNames: IconNames = ["home", "clone", "add-square", "notification"];
+  const tabPaths = ["/", "/feeds", "/newpost", "/notification"];
+
+  const handleTapChange = (idx: number) => {
+    if (pathname === tabPaths[idx]) return;
+    router.push(tabPaths[idx]);
+  };
+
+  return (
+    <footer className="flex fixed bottom-0 left-0 w-full h-16 bg-white">
+      {imageNames.map((imageName, idx) => (
+        <button
+          key={imageName}
+          className="flex justify-center items-center w-1/4 border-none bg-transparent p-0"
+          onClick={() => handleTapChange(idx)}>
+          <Image
+            src={`/footerIcons/${imageName}-${
+              pathname === tabPaths[idx] ? "on" : "off"
+            }.svg`}
+            alt={`${imageName}-icon`}
+            width={30}
+            height={30}
+          />
+        </button>
+      ))}
+    </footer>
+  );
+};
+
+export default Footer;
