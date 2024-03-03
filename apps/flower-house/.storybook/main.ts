a92a51c6a527043860d33fe0a669ea87bd8da8ce
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-03-04 00:04:41 +0900

    feat: storybook/addon 추가 및 설정 수정
    viewport : 화면 조절 기능,
    a11y : 웹 접근성 검증,
    design : figma 기능 추가,
    storysource : story 객체 코드 확인
    
    staticDirs 설정 추가 : 정적파일 인식을 위함
    appDirectory & decorators 설정 추가 : app rotuer를 고려하여 추가하였음

--- apps/flower-house/.storybook/main.ts
+++ apps/flower-house/.storybook/main.ts
@@ -1,6 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-
-import { join, dirname } from "path";
+import path, { dirname, join } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -12,13 +11,29 @@ function getAbsolutePath(value: string): any {
 const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-interactions")
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-viewport"),
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-designs"),
+    {
+      // @storybook/addon-storysource 설정을 객체 형태로 추가합니다.
+      name: getAbsolutePath("@storybook/addon-storysource"),
+      options: {
+        rule: {
+          include: [path.resolve(__dirname, "../src")] // src 디렉토리를 지정합니다.
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false } // Prettier 설정을 지정합니다.
+        }
+      }
+    }
   ],
   framework: {
     name: getAbsolutePath("@storybook/nextjs"),
@@ -26,6 +41,7 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag"
-  }
+  },
+  staticDirs: ["../public"]
 };
 export default config;
--- apps/flower-house/.storybook/preview.ts
+++ apps/flower-house/.storybook/preview.ts
@@ -1,4 +1,10 @@
+import {
+  INITIAL_VIEWPORTS,
+  MINIMAL_VIEWPORTS
+} from "@storybook/addon-viewport";
 import type { Preview } from "@storybook/react";
+import withAppRouterContext from "../app/lib/with-app-router-context";
+import "../app/ui/shared/globals.css";
 
 const preview: Preview = {
   parameters: {
@@ -8,8 +14,19 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i
       }
+    },
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...MINIMAL_VIEWPORTS
+      },
+      defaultViewport: "iphone14promax"
+    },
+    nextjs: {
+      appDirectory: true
     }
-  }
+  },
+  decorators: [withAppRouterContext]
 };
 
 export default preview;
