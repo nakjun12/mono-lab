607060f0e52af6ea63ba880c1b6bd57deb312208
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-03-09 18:47:38 +0900

    fix: 마커 설정 오버레이에서 초기 설정으로 이동

--- apps/flower-house/app/(router)/map/components/map-initializer.tsx
+++ apps/flower-house/app/(router)/map/components/map-initializer.tsx
@@ -6,16 +6,25 @@ import useMap, {
   INITIAL_CENTER,
   INITIAL_ZOOM
 } from "@/app/(router)/map/hooks/use-map"; // 임포트 경로 수정
+import useMarkers from "@/app/(router)/map/hooks/use-markers";
 import getCurrentLocation from "@/app/(router)/map/lib/get-current-location";
+import { MARKERS } from "@/app/shared/lib/dummy";
 import type { Coordinates, Map } from "@/app/shared/types/map-types";
 import { useSearchParams } from "next/navigation";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 //맵 초기 설정 및 마커 렌더링 컴포넌트
 const MapInitializer = () => {
+  const { initializeMarkers } = useMarkers();
   const { initializeMap } = useMap();
   //const { clearCurrentMarker } = useCurrentMarker();
   const searchParams = useSearchParams(); // 현재 URL의 검색 매개변수
+
+  useEffect(() => {
+    //초기 마커 더미로 설정함
+    //추후 서버로 업데이트 예정
+    initializeMarkers(MARKERS); //전역 상태 업데이트
+  }, [initializeMarkers]);
 
   //초기 줌을 설정합니다.
   const initialZoom = useMemo(
--- apps/flower-house/app/(router)/map/components/map-overlay.tsx
+++ apps/flower-house/app/(router)/map/components/map-overlay.tsx
@@ -2,26 +2,16 @@
 
 import useCopyAndReplaceMap from "@/app/(router)/map/hooks/use-copy-and-replace-map";
 import useMap from "@/app/(router)/map/hooks/use-map";
-import useMarkers from "@/app/(router)/map/hooks/use-markers";
-import { MARKERS } from "@/app/shared/lib/dummy";
-import { useEffect } from "react";
 
-import React, { ReactNode } from "react";
+import { FC, ReactNode } from "react";
 
 interface MapOverlayProps {
   children: ReactNode;
 }
 
-const MapOverlay: React.FC<MapOverlayProps> = ({ children }) => {
-  const { initializeMarkers } = useMarkers();
+const MapOverlay: FC<MapOverlayProps> = ({ children }) => {
   const copyAndReplaceMap = useCopyAndReplaceMap();
   const { resetMapOptions } = useMap();
-
-  useEffect(() => {
-    //초기 마커 더미로 설정함
-    //추후 서버로 업데이트 예정
-    initializeMarkers(MARKERS); //전역 상태 업데이트
-  }, [initializeMarkers]);
 
   return (
     <div>
