1123697d7b35c7b9c338e3a2f2acf43b66e93d03
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-03-12 09:32:45 +0900

    feat: msw 초기 세팅, handlers 미설정하였고, 그대로 가져왔음

---
+++ apps/flower-house/app/shared/components/msw-component.ts
@@ -0,0 +1,16 @@
+"use client";
+import { useEffect } from "react";
+
+export const MSWComponent = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      console.log(process.env.NEXT_PUBLIC_API_MOCKING);
+      if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+        console.log("mock server is running");
+        require("@/app/shared/mocks/browser");
+      }
+    }
+  }, []);
+
+  return null;
+};
