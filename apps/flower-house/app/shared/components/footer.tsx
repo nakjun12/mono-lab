310b4774561adff43f92079b220c662e6ac44a75
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-03-15 09:26:18 +0900

    feat: msw 통합 초기 세팅, webpack 수정으로 인해 리스크 있음

--- apps/flower-house/app/shared/components/footer.tsx
+++ apps/flower-house/app/shared/components/footer.tsx
@@ -4,11 +4,18 @@ import { usePathname, useRouter } from "next/navigation";
 
 import type { IconNames } from "../types/footer-type";
 
+import { useState } from "react";
 import FileInput from "./form/file-input";
+
+export type Movie = {
+  id: string;
+  title: string;
+};
 
 const Footer: React.FC = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const [movies, setMovies] = useState<Array<Movie>>([]);
 
   const imageNames: IconNames = ["home", "clone", "add-square", "notification"];
   const tabPaths = ["/", "/feeds", "/newpost", "/notification"];
@@ -19,13 +26,37 @@ const Footer: React.FC = () => {
     router.push(tabPaths[idx]);
   };
 
+  const fetchMovies = () => {
+    fetch("/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        query: `
+          query ListMovies {
+            movies {
+              id
+              title
+            }
+          }
+        `
+      })
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch(() => setMovies([]));
+  };
+
   return (
     <footer className="flex fixed bottom-0 left-0 w-full h-16 bg-white">
       {imageNames.map((imageName, idx) => (
         <button
           key={imageName}
           className="relative flex justify-center items-center w-1/4 border-none bg-transparent p-0"
-          onClick={() => handleTapChange(idx)}
+          onClick={fetchMovies}
         >
           {idx === 2 && <FileInput router={router} />}
           <Image
--- apps/flower-house/next.config.mjs
+++ apps/flower-house/next.config.mjs
@@ -9,6 +9,31 @@ const nextConfig = {
         pathname: "/**" // 모든 경로를 허용하는 와일드카드 패턴
       }
     ]
+  },
+  experimental: {
+    instrumentationHook: true
+  },
+  webpack(config, { isServer }) {
+    /**
+     * @fixme This is completely redundant. webpack should understand
+     * export conditions and don't try to import "msw/browser" code
+     * that's clearly marked as client-side only in the app.
+     */
+    if (isServer) {
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: "msw/browser", alias: false });
+      } else {
+        config.resolve.alias["msw/browser"] = false;
+      }
+    } else {
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: "msw/node", alias: false });
+      } else {
+        config.resolve.alias["msw/node"] = false;
+      }
+    }
+
+    return config;
   }
 };
 
