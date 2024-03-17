2636d396a6b7ab7690393171bf7b2189d7e5d60b
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-03-18 08:34:50 +0900

    fix: storybook import 경로 수정

--- apps/flower-house/.storybook/preview.ts
+++ apps/flower-house/.storybook/preview.ts
@@ -3,8 +3,8 @@ import {
   MINIMAL_VIEWPORTS
 } from "@storybook/addon-viewport";
 import type { Preview } from "@storybook/react";
-import "../app/core/globals.css";
-import withAppRouterContext from "../app/core/libs/with-app-router-context";
+import "../app/core/shared/globals.css";
+import withAppRouterContext from "../app/core/shared/libs/with-app-router-context";
 
 const preview: Preview = {
   parameters: {
