8af5274d39203a77231c09635d49c424f8901ff2
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-03-20 09:29:03 +0900

    fix: card-carousel 초기 로드시에 캐러셀 업데이트 막음

--- apps/flower-house/app/core/map/components/card-carousel.tsx
+++ apps/flower-house/app/core/map/components/card-carousel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import useCurrentSlideIndex from "@/app/core/map/hooks/use-current-slide-index";
-import { ComponentType, useEffect, useRef } from "react";
+import { ComponentType, useEffect, useRef, useState } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -40,28 +40,23 @@ const CardCarousel = <T extends { id: string | number }>({
 }: CardCarouselProps<T>) => {
   //TODO: 데이터 업데이트시에 마운트되어서 해당 주변 데이터 받아오도록
   const swiperRef = useRef<SwiperRef>(null);
+  const isInitLoadRef = useRef(false);
+  const [isInitialLoad, setIsInitialLoad] = useState(false);
   const { currentSlideIndex } = useCurrentSlideIndex();
 
   useEffect(() => {
     if (currentSlideIndex === undefined) return;
-
-    setTimeout(() => {
-      const isBoolean =
-        swiperRef.current?.swiper.slideToLoop(currentSlideIndex);
-      console.log(isBoolean, currentSlideIndex, "swiper.realIndex222");
-    }, 10);
+    setIsInitialLoad(true);
+    isInitLoadRef.current = true;
+    const isBoolean = swiperRef.current?.swiper.slideToLoop(currentSlideIndex);
   }, [currentSlideIndex]);
 
   const handleSlideChange = (swiper: {
     realIndex: number;
     activeIndex: number;
   }) => {
-    console.log(
-      swiper.realIndex,
-      "swiper.realIndex222 슬라이드 변화",
-      swiper.activeIndex,
-      currentSlideIndex
-    );
+    if (!isInitLoadRef.current) return;
+
     onActiveSlideChange?.(swiper.realIndex);
     // 슬라이드 변경이 사용자의 직접적인 액션에 의한 것이라면, onActiveSlideChange를 호출합니다.
   };
@@ -95,7 +90,10 @@ const CardCarousel = <T extends { id: string | number }>({
         spaceBetween={5}
         autoplay={isAutoplay}
         onSlideChange={handleSlideChange}
-        onSwiper={(swiper) => console.log(swiper, "swiper")}
+        onSliderFirstMove={() => {
+          console.log("slider first move");
+          isInitLoadRef.current = true; // Swiper 초기화 시 초기 로드 상태를 false로 설정
+        }}
       >
         {data?.map((data) => (
           /*첫화면 레이지 로딩 걸릴 경우 오류 발생하니 주의할 것 */
