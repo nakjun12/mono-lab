963a26ae01de882accda2f071a7ace51901aada5
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-04-05 22:06:39 +0900

    fix: vitest.config 절대경로 추가

--- apps/flower-house/vitest.config.ts
+++ apps/flower-house/vitest.config.ts
@@ -1,12 +1,16 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./")
+    }
+  },
   test: {
     environment: "jsdom",
     exclude: ["**/e2e/**", "**/node_modules/**"]
   }
 });
-
-//ex) https://github.com/vercel/next.js/tree/canary/examples/with-vitest
