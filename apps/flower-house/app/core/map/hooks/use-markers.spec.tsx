6ce4e5c446d868286d5dd017cb84290076e973ce
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-04-08 01:08:44 +0900

    fix(use-markers): markers type 수정

--- apps/flower-house/app/core/map/hooks/use-markers.spec.tsx
+++ apps/flower-house/app/core/map/hooks/use-markers.spec.tsx
@@ -1,6 +1,21 @@
+import type { Marker } from "@/app/core/shared/types/map-types";
 import { renderHook } from "@testing-library/react-hooks";
 import { describe, expect, it } from "vitest";
 import useMarkers from "./use-markers";
+
+const newMarkers: Marker[] = [
+  {
+    id: "marker-1",
+    title: "긴고랑로의 봄 꽃",
+    address: "긴고랑로",
+    type: "forsythia",
+    thumbnail: "/example.png",
+    coordinates: [37.5630685, 127.086228], // 튜플 형태로 수정
+    category: "Wildflowers",
+    likes: 100,
+    comments: 15
+  }
+];
 
 describe("useMarkers 훅 테스트", () => {
   it("초기에는 마커 데이터가 존재하지 않는다.", async () => {
@@ -10,7 +25,6 @@ describe("useMarkers 훅 테스트", () => {
   });
 
   it("updateMarkers 함수를 호출하여 마커 데이터를 설정할 수 있다", async () => {
-    const newMarkers = [{ id: "3", name: "Marker 3" }];
     const { result, waitFor } = renderHook(() => useMarkers());
 
     await result.current.updateMarkers(newMarkers);
@@ -19,15 +33,21 @@ describe("useMarkers 훅 테스트", () => {
   });
 
   it("updateMarkers 함수를 호출하여 설정한 마커 데이터를 수정할 수 있다", async () => {
-    const newMarkers = [{ id: "3", name: "Marker 3" }];
     const { result, waitFor } = renderHook(() => useMarkers());
 
     await result.current.updateMarkers(newMarkers);
 
     await waitFor(() => expect(result.current.markers).toEqual(newMarkers));
 
-    const updateMarkers = [{ id: "4", name: "Marker 4" }];
-    await result.current.updateMarkers(updateMarkers);
-    await waitFor(() => expect(result.current.markers).toEqual(updateMarkers));
+    const modifiedMarkers =
+      result.current.markers?.map((marker) => ({
+        ...marker,
+        likes: marker.likes + 1 // 예시: 모든 마커의 'likes'를 1 증가
+      })) || [];
+
+    await result.current.updateMarkers(modifiedMarkers);
+    await waitFor(() =>
+      expect(result.current.markers).toEqual(modifiedMarkers)
+    );
   });
 });
