780d3d5a9d7a2594fe1ede3147baf3ba95ab8764
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-04-10 03:42:51 +0900

    feat(e-commerce): react-query-provider 추가

--- apps/e-commerce/app/(router)/layout.tsx
+++ apps/e-commerce/app/(router)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "~/app/core/shared/globals.css";
-
+import ReactQueryProvider from "~/app/core/shared/hook/react-query-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +16,9 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
