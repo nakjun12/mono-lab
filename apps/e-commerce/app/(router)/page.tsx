9df91b58a713f36f412602cc0613b8aa50039272
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-04-10 03:12:45 +0900

    remove(e-commerce): 폴더 구조 수정

--- apps/e-commerce/app/(router)/page.tsx
+++ apps/e-commerce/app/(router)/page.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
+import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
 import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import Image from "next/image";
+import styles from "~/app/core/shared/page.module.css";
 
 function Gradient({
   conic,
   className,
-  small,
+  small
 }: {
   small?: boolean;
   conic?: boolean;
@@ -19,7 +19,7 @@ function Gradient({
         styles.gradient,
         conic ? styles.glowConic : undefined,
         small ? styles.gradientSmall : styles.gradientLarge,
-        className,
+        className
       ]
         .filter(Boolean)
         .join(" ")}
@@ -31,24 +31,24 @@ const LINKS = [
   {
     title: "Docs",
     href: "https://turbo.build/repo/docs",
-    description: "Find in-depth information about Turborepo features and API.",
+    description: "Find in-depth information about Turborepo features and API."
   },
   {
     title: "Learn",
     href: "https://turbo.build/repo/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
+    description: "Learn more about monorepos with our handbook."
   },
   {
     title: "Templates",
     href: "https://turbo.build/repo/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
+    description: "Choose from over 15 examples and deploy with a single click."
   },
   {
     title: "Deploy",
     href: "https://vercel.com/new",
     description:
-      "Instantly deploy your Turborepo to a shareable URL with Vercel.",
-  },
+      "Instantly deploy your Turborepo to a shareable URL with Vercel."
+  }
 ];
 
 export default function Page(): JSX.Element {
@@ -63,8 +63,7 @@ export default function Page(): JSX.Element {
           <a
             href="https://vercel.com?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"
             rel="noopener noreferrer"
-            target="_blank"
-          >
+            target="_blank">
             By{" "}
             <Image
               alt="Vercel Logo"
@@ -115,8 +114,7 @@ export default function Page(): JSX.Element {
               className={styles.turborepoWordmark}
               viewBox="0 0 506 50"
               width={200}
-              xmlns="http://www.w3.org/2000/svg"
-            >
+              xmlns="http://www.w3.org/2000/svg">
               <title>Turborepo logo</title>
               <path d="M53.7187 12.0038V1.05332H0.945312V12.0038H20.8673V48.4175H33.7968V12.0038H53.7187Z" />
               <path d="M83.5362 49.1431C99.764 49.1431 108.67 40.8972 108.67 27.3081V1.05332H95.7401V26.0547C95.7401 33.6409 91.7821 37.9287 83.5362 37.9287C75.2904 37.9287 71.3324 33.6409 71.3324 26.0547V1.05332H58.4029V27.3081C58.4029 40.8972 67.3084 49.1431 83.5362 49.1431Z" />
