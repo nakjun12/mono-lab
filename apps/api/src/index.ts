0def4fb48401ee68e49174e71203c48b17a1ebfc
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-07-31 00:47:01 +0900

    remove: express 제거

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { createServer } from "./server";
-const port = process.env.PORT || 3001;
-const server = createServer();
-
-server.listen(port, () => {
-	console.log(`api running on ${port}`);
-});
