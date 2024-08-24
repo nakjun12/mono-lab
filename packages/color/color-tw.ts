a449588641461ac277ac5cf118f8451a1fe38add
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-08-24 23:20:54 +0900

    feat: color.ts 파일 build 추가

---
+++ packages/color/color-tw.ts
@@ -0,0 +1,113 @@
+export const extendsTheme = {
+  colors: {
+    "tw-green": "#009c92",
+    "tw-green15": "rgba(0, 156, 146, 0.1)",
+    "tw-red": "#be3c30",
+    "tw-red15": "rgba(207, 46, 31, 0.1)",
+    "tw-yellow": "#FF9500",
+    "tw-yellow15": "rgba(238, 165, 28, 0.1)",
+    "tw-blue": "#006fe8",
+    "tw-blue15": "rgba(0, 111, 232, 0.1)",
+    "tw-pink": "#ff4568",
+    "tw-pink15": "rgba(255, 69, 104, 0.1)",
+    "tw-purple": "#B964E4",
+    "tw-purple15": "rgba(185, 100, 228, 0.1)",
+    "tw-skyBlue": "#09B2FF",
+    "tw-skyBlue15": "rgba(9, 178, 255, 0.1)",
+    "tw-lime": "#a2a600",
+    "tw-lime15": "rgba(162, 166, 0, 0.1)",
+    "tw-violet": "#8F78EE",
+    "tw-violet15": "rgba(143, 120, 238, 0.1)",
+    "tw-lightGreen": "#34C759",
+    "tw-lightGreen15": "rgba(52, 199, 89, 0.1)",
+
+    "tw-succeed3": "#00d398",
+    "tw-succeed2": "#00d398",
+    "tw-succeed1": "#d2fff2",
+    "tw-error3": "#ff5a4b",
+    "tw-error2": "#ff5a4b",
+    "tw-error1": "#ffe3e0",
+
+    "tw-background": "#e8edf4",
+
+    "tw-violet8": "#826cff",
+    "tw-violet7": "#a090ff",
+    "tw-violet6": "#bbb1ff",
+    "tw-violet5": "#e1dcff",
+    "tw-violet4": "#eae7ff",
+    "tw-violet3": "#f2f0ff",
+    "tw-violet2": "#f4f3fb",
+    "tw-violet1": "#fcfaff",
+
+    "tw-blue8": "#3b7ee3",
+    "tw-blue7": "#609fff",
+    "tw-blue6": "#8ebbff",
+    "tw-blue5": "#cadfff",
+    "tw-blue4": "#e2eeff",
+    "tw-blue3": "#eff7ff",
+    "tw-blue2": "#f6fbff",
+    "tw-blue1": "#fcfeff",
+
+    "tw-grey-scale10": "#383852",
+    "tw-grey-scale9": "#4d4d68",
+    "tw-grey-scale8": "#7e7e94",
+    "tw-grey-scale7": "#9f9fb7",
+    "tw-grey-scale6": "#c0c3d5",
+    "tw-grey-scale5": "#dadde9",
+    "tw-grey-scale4": "#edecf7",
+    "tw-grey-scale3": "#f4f6fb",
+    "tw-grey-scale2": "#f9fafd",
+    "tw-grey-scale1": "#ffffff",
+
+    transparent: "transparent"
+  },
+  fontSize: {
+    "tw-p5": ["11px", "16px"],
+    "tw-p4": ["12px", "18px"],
+    "tw-p3": ["13px", "20px"],
+    "tw-p2": ["14px", "22px"],
+    "tw-p1": ["15px", "24px"],
+    "tw-t2": ["18px", "26px"],
+    "tw-t1": ["20px", "30px"]
+  },
+  fontWeight: {
+    "tw-medium": "500", // medium
+    "tw-bold": "700" // bold
+  },
+  boxShadow: {
+    "tw-light": "0px 2px 5px rgba(0, 0, 0, 0.05)",
+    "tw-regular":
+      "0px 4px 10px rgba(0, 0, 0, 0.05), 0px 2px 5px rgba(0, 0, 0, 0.05)",
+    "tw-medium":
+      "0px 4px 20px 5px rgba(0, 0, 0, 0.05), 0px 3px 10px rgba(0, 0, 0, 0.1)",
+    "tw-bold":
+      "0px 4px 50px 5px rgba(0, 0, 0, 0.05), 1px 3px 20px rgba(0, 0, 0, 0.05), 0px 10px 30px rgba(0, 0, 0, 0.1)"
+  },
+  borderRadius: {
+    "tw-xs": "4px",
+    "tw-sm": "6px",
+    "tw-md": "8px",
+    "tw-lg": "12px",
+    "tw-xl": "16px"
+  },
+  spacing: {
+    "tw-0": "0px",
+    "tw-2": "2px",
+    "tw-4": "4px",
+    "tw-6": "6px",
+    "tw-8": "8px",
+    "tw-10": "10px",
+    "tw-12": "12px",
+    "tw-14": "14px",
+    "tw-16": "16px",
+    "tw-18": "18px",
+    "tw-20": "20px",
+    "tw-22": "22px",
+    "tw-24": "24px",
+    "tw-30": "30px",
+    "tw-32": "32px",
+    "tw-36": "36px",
+    "tw-40": "40px",
+    "tw-48": "48px"
+  }
+};
