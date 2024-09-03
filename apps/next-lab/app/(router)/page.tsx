f0884ada4f0f2fff0eaba73f3b2ee9841d602535
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-03 22:59:41 +0900

    feat: tailwindcss test

--- apps/next-lab/app/(router)/page.tsx
+++ apps/next-lab/app/(router)/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Page(): JSX.Element {
-  return <main>하이</main>;
+  const [test, setTest] = useState(false);
+  const text = "text-purple-300";
+  return (
+    <main className="bg-red">
+      <div
+        className={`bg-red ${text} ${test ? "bg-slate-300" : "bg-tw-green"} text-purple-${1 * 500}`}>
+        rkawk
+      </div>
+      하이
+    </main>
+  );
 }
