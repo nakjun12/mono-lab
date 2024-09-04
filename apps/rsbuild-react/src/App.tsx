a1e23e1af8b3dc053f1a8827ce89e94d32978ccc
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-04 23:02:28 +0900

    feat: tailwindCSS 설치 및 적용

--- apps/rsbuild-react/src/App.tsx
+++ apps/rsbuild-react/src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
+      <h1 className="">Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
