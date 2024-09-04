9d1b50c4ee563b2e5edb762c11aacb9a300ee953
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-04 22:39:36 +0900

    feat: react 19 및 compiler 추가

--- apps/rsbuild-react/rsbuild.config.ts
+++ apps/rsbuild-react/rsbuild.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginBabel } from "@rsbuild/plugin-babel";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+      babelLoaderOptions(opts) {
+        opts.plugins?.unshift("babel-plugin-react-compiler");
+      }
+    })
+  ]
 });
