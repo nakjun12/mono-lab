1c53df0c4d8707a8cb0495c418aae504e77c791c
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-13 23:01:03 +0900

    remove: flower house 삭제

--- apps/flower-house/app/(router)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/app/core/shared/components/form/login-form";
-
-export default function LoginPage() {
-  return (
-    <main className="flex items-center justify-center md:h-screen">
-      <LoginForm />
-    </main>
-  );
-}
--- apps/flower-house/app/(router)/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "@/app/core/shared/components/form/sign-up-form";
-
-export default function SignUpPage() {
-  return (
-    <main className="flex items-center justify-center md:h-screen">
-      <SignUpForm />
-    </main>
-  );
-}
