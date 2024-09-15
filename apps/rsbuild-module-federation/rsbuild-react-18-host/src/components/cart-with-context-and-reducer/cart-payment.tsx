7fc9723673212c012c2cf374522b38f061c2df6d
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-15 16:46:17 +0900

    fix:cart header 및 cart payment 수정

--- apps/rsbuild-module-federation/rsbuild-react-18-host/src/components/cart-with-context-and-reducer/cart-payment.tsx
+++ apps/rsbuild-module-federation/rsbuild-react-18-host/src/components/cart-with-context-and-reducer/cart-payment.tsx
@@ -2,6 +2,23 @@ import type React from "react";
 import { useCartState } from "./hooks/useCart";
 
 export const CartPayment: React.FC = () => {
+  return (
+    <div className="mt-8 p-4 bg-gray-100 rounded">
+      <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
+      <div className="flex justify-between items-center">
+        <span>Total:</span>
+        <Price />
+      </div>
+      <button
+        type="button"
+        className="mt-4 w-full py-2 bg-green-500 text-white rounded hover:bg-green-600 transition-colors">
+        Proceed to Checkout
+      </button>
+    </div>
+  );
+};
+
+const Price = () => {
   const { items } = useCartState();
 
   const totalAmount = items.reduce(
@@ -10,19 +27,6 @@ export const CartPayment: React.FC = () => {
   );
 
   return (
-    <div className="mt-8 p-4 bg-gray-100 rounded">
-      <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
-      <div className="flex justify-between items-center">
-        <span>Total:</span>
-        <span className="text-2xl font-bold">
-          {totalAmount.toLocaleString()}원
-        </span>
-      </div>
-      <button
-        type="button"
-        className="mt-4 w-full py-2 bg-green-500 text-white rounded hover:bg-green-600 transition-colors">
-        Proceed to Checkout
-      </button>
-    </div>
+    <span className="text-2xl font-bold">{totalAmount.toLocaleString()}원</span>
   );
 };
