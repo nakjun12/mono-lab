3fa33c99130cd79b3d9963fc86cc3f9bac887902
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-09-15 12:26:08 +0900

    chore: 미사용 코드 삭제

--- apps/rsbuild-react-19/src/App.tsx
+++ apps/rsbuild-react-19/src/App.tsx
@@ -4,7 +4,6 @@ import { Home } from "./pages/home";
 const App = () => {
   return (
     <main className="content">
-      <h1 className="bg-red-50">Rsbuild with React</h1>
       <Home />
     </main>
   );
