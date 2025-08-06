2795017cc8bff1982fc2f27e2932c176c084af38
nakjun12 <wns450@gmail.com>
2025-08-06 18:02:44 +0900

    test: 주석 추가

--- apps/project/nest-api/src/app.module.ts
+++ apps/project/nest-api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//주석 추가
 @Module({
   imports: [],
   controllers: [AppController],
